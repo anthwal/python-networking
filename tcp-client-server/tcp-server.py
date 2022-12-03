f15df8df5507be886e041cc62a15471093cfae29
anthwal <anthwalarun003@gmail.com>
2022-12-03 12:14:54 +0530

    tcp-client-server example

---
+++ tcp-client-server/tcp-server.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+
+IP = "0.0.0.0"
+PORT = 9998
+
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((IP, PORT))
+    server.listen(5)
+    print(f"[*] Listening on {IP}:{PORT}")
+    # handler loop of the server, it sends each incoming connection
+    # to a separate thread to be handled
+    while True:
+        client, address = server.accept()
+        print(f"[*] Accepted connection from {address[0]}:{address[1]}")
+        client_handler = threading.Thread(target=handle_client, args=(client,))
+        client_handler.start()
+
+
+def handle_client(client_socket):
+    """
+    function to handle request from the client connected, receives data
+    from the client and sends example acknowledgement string back to
+    the client"""
+    with client_socket as sock:
+        request = sock.recv(4096)
+        print(f'[*] Received: {request.decode("utf-8")}')
+        sock.send(b"ACK")
+
+
+if __name__ == "__main__":
+    main()
